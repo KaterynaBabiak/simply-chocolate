b6b1303100299603beae87a685698a83c191c5bd
KaterynaBabiak <124308784+KaterynaBabiak@users.noreply.github.com>
2023-10-11 13:42:54 +0000

    Deploying to gh-pages from @ KaterynaBabiak/simply-chocolate@6641e63e6e368ad000456bfbdca1ffe98befaa57 🚀

---
+++ assets/swiper-our-products-464f24f4.js
@@ -0,0 +1,1 @@
+new Swiper(".slide-content",{slidesPerView:4,centerSlide:"true",fade:"true",grabCursor:"true",pagination:{el:".swiper-pagination",clickable:!0}});
---
+++ assets/swiper-rewiev-f4d9451b.js
@@ -0,0 +1,1 @@
+new Swiper(".review-swiper",{spaceBetween:16,grabCursor:!0,slidesPerView:"auto",direction:"horizontal",pagination:{el:".swiper-pagination",clickable:!0},breakpoints:{320:{slidesPerView:1,spaceBetween:18},768:{slidesPerView:2,spaceBetween:16},1200:{slidesPerView:3,spaceBetween:28}}});
