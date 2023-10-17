3de8ada9e28f9148bc1303d43b4758830091506a
Kateryna <katerynababiak@gmail.com>
2023-10-17 17:04:30 +0300

    commit new changes

--- src/main.js
+++ src/main.js
@@ -1,2 +1,6 @@
-import ('./js/swiper-our-products.js');
-import ('./js/swiper-rewiev.js');
+import './js/swiper-our-products.js';
+import './js/swiper-rewiev.js';
+import './js/scroll.js';
+import './js/modal-buyNow.js';
+import './js/modal-reviews.js';
+import './js/modal-subscribe.js';
